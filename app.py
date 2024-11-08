d73c15415583e2ce00126a00ab093a10e1910c36
Your NameEyepatch420 <wwwamaanansari0@gmail.com>
2024-11-08 21:30:07 +0530

    first

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def api_colleges():
 
 if __name__ == '__main__':
     # Ensure Flask uses the correct port set by Render (or default to port 5000)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+     app.run(debug=True)
+
